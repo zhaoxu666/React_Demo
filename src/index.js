6d303f8444195145f96d25d10665c2eabc029c5f
victor <cbcbj.zhaoxu@cbcbeijing.net>
2019-03-29 15:15:46 +0800

    实现React Render函数

--- src/index.js
+++ src/index.js
@@ -1,5 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoApp  from "./Components/TodoApp";
 //同步测试
-ReactDOM.render(<TodoApp />, document.getElementById('root'));
+//ReactDOM.render(<TodoApp />, document.getElementById('root'));
+//render函数原理
+let ele = <h1>hello world</h1>
+console.log(ele);
+//最终的React元素是这个样子的。
+let eleObj = {
+    type:"h1",
+    props:{
+        className:'red',
+        id:1,
+        children:["hello",{
+            type:"span",
+            props:{
+                className:'green',
+                children:["world"]
+            }
+        }]
+    }
+    
+}
+function render(eleObj,container){
+    let {type,props} = eleObj;
+    let ele = document.createElement(type);
+    for(let attr in props){
+        if(attr == 'className'){
+            ele.setAttribute('class',props[attr]);
+        }
+        else if(attr == 'children'){
+            props[attr].forEach(element => {
+                if(typeof element == 'string'){
+                let texNode = document.createTextNode(element);
+                ele.appendChild(texNode);
+            }
+            else{
+                render(element,ele);
+            }
+            });
+        }
+        else{
+            ele.setAttribute(attr,props[attr]);
+        }
+    };
+    container.appendChild(ele);
+}
+
+render(eleObj,document.getElementById('root'))
